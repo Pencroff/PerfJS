18c2ff1a84242ceeb6e4d680cb94659ce8e4ea06
Sergey Danilov <pencroff@gmail.com>
2016-09-04 18:39:45 +0100

    generation data file

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,54 @@
  */
 
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var dotify = require('gulp-dotify');
-var header = require('gulp-header');
 var watch = require('gulp-watch');
+var transform = require('gulp-transform');
+var wrapper = require('gulp-wrapper');
 
 var destination = 'assets';
 
+gulp.task('generate-data-file', function() {
+    global.window = {};
+    return gulp.src('tests/*.js')
+        .pipe(plumber())
+        .pipe(transform(function (content) {
+            eval(content);
+            var test = global.window.test;
+            return [
+                '{ ',
+                    'id:"', test.id, '",',
+                    'name:"', test.name, '",',
+                    'description:"', test.description, '",',
+                    'tags:', '["', test.tags.join('","'), '"],',
+                    'url:"', test.url, '"',
+                ' }'
+            ].join('');
+        }, {encoding: 'utf8'}))
+        .pipe(concat('data.js', {newLine: ','}))
+        .pipe(wrapper({
+            header: '(function (r) {r.data=[',
+            footer: '];})(window.PerformanceJs);'
+        }))
+        .pipe(gulp.dest(destination));
+});
+
 gulp.task('templates', function() {
     return gulp.src('src/views/**/*.html')
+        .pipe(plumber())
         .pipe(dotify({
-            dictionary: 'PerformanceJs.JST'
+            dictionary: 'r.JST'
         }))
         .pipe(concat('templates.min.js'))
         .pipe(uglify())
-        .pipe(header('window.PerformanceJs.JST = {};'))
+        .pipe(wrapper({
+            header: '(function (r) {r.JST={},',
+            footer: '})(window.PerformanceJs);'
+        }))
         .pipe(gulp.dest(destination));
 });
 
@@ -57,6 +88,7 @@ gulp.task('engine-watch', ['app-watch', 'templates-watch']);
 function makeTask(options) {
     gulp.task(options.name, function() {
         var stream = gulp.src(options.files)
+            .pipe(plumber())
             .pipe(concat(options.name + '.js'))
             .pipe(gulp.dest(options.destination));
         if (options.uglify) {
